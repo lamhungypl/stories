7b344a41fe43f4cdf0ebbec7941d3026c2f44855
lamhungypl <lamhungypl@gmail.com>
2023-01-18 13:49:18 +0700

    stuff

--- src/chunks/someChunk3/chunk_StoriesGradientBackground.react.js
+++ src/chunks/someChunk3/chunk_StoriesGradientBackground.react.js
@@ -4,13 +4,13 @@ __d(
   function (a, b, c, d, e, f, g) {
     'use strict';
     var h = d('react'),
-      i = d('react').useEffect;
-    function a(a) {
-      var b = a.direction,
-        c = a.from,
-        e = a.onLoad;
-      a = a.to;
-      i(
+      useEffect = d('react').useEffect;
+    function Component(a) {
+      let b = a.direction;
+      let c = a.from;
+      let e = a.onLoad;
+      let a = a.to;
+      useEffect(
         function () {
           e && e();
         },
@@ -21,35 +21,41 @@ __d(
       if (!(a != null && c != null)) return null;
       a = a != null ? a : c;
       c = c != null ? c : a;
-      var f = '';
+      let gradientDirection = '';
       switch (b) {
         case d('StoriesEnums').GRADIENT_DIRECTION.BL_TR:
         case d('StoriesEnums').GRADIENT_DIRECTION.TR_BL:
-          f = '45deg';
+          gradientDirection = '45deg';
           break;
         case d('StoriesEnums').GRADIENT_DIRECTION.LEFT_RIGHT:
         case d('StoriesEnums').GRADIENT_DIRECTION.RIGHT_LEFT:
-          f = 'to right';
+          gradientDirection = 'to right';
           break;
         case d('StoriesEnums').GRADIENT_DIRECTION.TL_BR:
         case d('StoriesEnums').GRADIENT_DIRECTION.BR_TL:
-          f = '135deg';
+          gradientDirection = '135deg';
           break;
         case d('StoriesEnums').GRADIENT_DIRECTION.TOP_BOTTOM:
         case d('StoriesEnums').GRADIENT_DIRECTION.BOTTOM_TOP:
-          f = 'to bottom';
+          gradientDirection = 'to bottom';
           break;
         default:
-          f = '0deg';
+          gradientDirection = '0deg';
       }
-      b = {
+      let style = {
         backgroundImage:
-          'linear-gradient(' + f + ',' + c + ' 0%,' + a + ' 100%)',
+          'linear-gradient(' +
+          gradientDirection +
+          ',' +
+          c +
+          ' 0%,' +
+          a +
+          ' 100%)',
       };
-      return h.jsx('div', { className: 'x5yr21d xh8yej3', style: b });
+      return h.jsx('div', { className: 'x5yr21d xh8yej3', style: style });
     }
-    a.displayName = a.name + ' [from ' + f.id + ']';
-    g['default'] = a;
+    Component.displayName = Component.name + ' [from ' + f.id + ']';
+    g['default'] = Component;
   },
   98,
 );
